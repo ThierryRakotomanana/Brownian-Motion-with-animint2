3426e302497d881490bd884db59fb6e625c135f7
Thierry Rakotomanana <ThierryRktm@gmail.com>
2023-03-28 18:30:21 +0300

    r code for BM in animint2

---
+++ BM.r
@@ -0,0 +1,26 @@
+
+brownian_anim <- function() {
+  ani.options(interval = 0.05, nmax = 150)
+   # nolint
+  x <- y <- rep(0, 10)
+  dat <- data.frame(x, y)
+   # nolint: trailing_whitespace_linter.
+  make_brownian_step <- function(x, y) {
+    x <- x + rnorm(1)
+    y <- y + rnorm(1)
+    return(data.frame(x, y))
+  }
+  
+  make_brownian_path <- function(i) {
+    dat <<- rbind(dat, make_brownian_step(dat$x[i], dat$y[i]))
+    return(dat)
+  }
+  
+  ggplot2::ggplot(dat, ggplot2::aes(x, y)) + 
+    ggplot2::geom_point(shape = 21, size = 5, color = "red", fill = "yellow") + 
+    ggplot2::labs(title = "Demonstration of Brownian Motion") +
+    transition_reveal(seq_along(dat$x))
+}
+
+# Print the animint2 plot
+print(animint(brownian_anim))
